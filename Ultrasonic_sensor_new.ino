06e34d3a0f9a564d0e3d1961791364b09cbd0fe5
Rujuta-k <rujuta.kulkarni12@gmail.com>
2025-02-14 21:21:41 +0530

    Update Ultrasonic_sensor_new.ino

--- Ultrasonic_sensor_new.ino
+++ Ultrasonic_sensor_new.ino
@@ -18,7 +18,7 @@ void loop()
   digitalWrite(TRIG,LOW);
   duration= pulseIn(ECHO,HIGH); 
   distance = duration*(0.034 / 2.0);
-  int coordinates = map(distance,0,1023,0,255);
+  int coordinates = map(distance,0,1023,255,0);
   analogWrite(LED,coordinates); 
   Serial.print(distance);
   Serial.println("cm");
