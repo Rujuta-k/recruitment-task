ab9834cfc25fad9f8f82b3534eb9845a23cd380a
Rujuta-k <rujuta.kulkarni12@gmail.com>
2025-02-14 19:21:32 +0530

    Add files via upload

---
+++ Ultrasonic_sensor_new.ino
@@ -0,0 +1,29 @@
+#define TRIG D1
+#define ECHO D2
+#define LED D0
+long duration; 
+int distance; 
+void setup(){
+  pinMode(TRIG, OUTPUT);
+  pinMode(LED,OUTPUT);
+  pinMode(ECHO,INPUT);
+  Serial.begin(9600);
+}
+void loop()
+{
+  digitalWrite(TRIG,LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG,HIGH);
+  delay(10);
+  digitalWrite(TRIG,LOW);
+  duration= pulseIn(ECHO,HIGH); 
+  distance = duration*(0.034 / 2.0);
+  int coordinates = map(distance,0,1023,0,255);
+  analogWrite(LED,coordinates); 
+  Serial.print(distance);
+  Serial.println("cm");
+  Serial.println();
+  
+  delay(100);
+
+}
